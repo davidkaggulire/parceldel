81e1ba6605090171e42a29e28f8b37d6cf181606
davidkaggulire <dkaggulire@gmail.com>
2019-02-12 13:08:59 +0300

    [Chore]Create dev environments

--- config.py
+++ config.py
@@ -1,1 +1,18 @@
 """config.py"""
+
+class Config:
+    """parent config class"""
+    DEBUG = False
+
+class TestingConfig(Config):
+    """Testing config class"""
+    DEBUG = True
+
+class DevelopmentConfig(Config):
+    """development config class"""
+    DEBUG = False
+
+app_config = {
+    "testing_config": TestingConfig,
+    "development_config": DevelopmentConfig
+}
