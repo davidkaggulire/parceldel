15e8cd2ed20d59e2391ec7fb6875285bc565c35f
davidkaggulire <dkaggulire@gmail.com>
2019-02-12 14:13:23 +0300

    Creates test file configurations

---
+++ tests/__init__.py
@@ -0,0 +1,43 @@
+"""__init__.py"""
+
+parcel_one = {
+	"parcel_name": "stapler",
+	"user_id": 2,
+	"quantity": 15,
+	"price": 1500
+}
+
+parcel_two = {
+	"parcel_name": "books",
+	"user_id": 2,
+	"quantity": 15,
+	"price": 1500
+}
+
+parcel_three = {
+	"parcel_name": "geometry set",
+	"user_id": 1,
+	"quantity": 2,
+	"price": 1500
+}
+
+parcel_list =[
+    {
+        "parcel_name": "stapler",
+        "user_id": 2,
+        "quantity": 15,
+        "price": 1500
+    },
+    {
+        "parcel_name": "books",
+        "user_id": 2,
+        "quantity": 15,
+        "price": 1500
+    },
+    {
+        "parcel_name": "geometry set",
+        "user_id": 1,
+        "quantity": 2,
+        "price": 1500
+    }
+]
---
+++ tests/test_parcels.py
@@ -0,0 +1,4 @@
+"""test_parcels.py"""
+
+import unittest
+from api.v1 import create_app
