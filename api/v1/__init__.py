c2c27784ebece9d1474e8ea32167176c1bc5e0b5
davidkaggulire <dkaggulire@gmail.com>
2019-02-12 14:09:05 +0300

    Edits init file

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -1,7 +1,12 @@
 """__init__.py"""
 
 from flask import Flask
+from config import app_config
+from api.v1.views.parcel_views import order
 
-app = Flask(__name__)
 
-from api.v1.views import views
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(app_config[config_name])
+    app.register_blueprint(order)
+    return app
